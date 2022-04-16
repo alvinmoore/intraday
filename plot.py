db14ab27d2862d2e82594b5fd33d846188df4fc2
Alvin Moore <alvinm@apple.com>
2022-04-16 10:01:37 -0600

    Added support for saving last year graph Removed tracking backup files Added displaying date of image range

--- plot.py
+++ plot.py
@@ -6,9 +6,9 @@ from datetime import datetime, timedelta, date
 
 STOCKS = [ 'UVXY', '^GSPC', '^VIX', 'AMC', 'GME', 'AAPL', '^SPX', 'TSLA', '^DJI', 'SNP', 'GOOG', 'FB', 'SNOW', 'NFLX' ]
 #STOCKS = [ 'UVXY' ]
-DAYS_FILTERS = { "today" : 1, "lastweek" : 7, "lastmonth" : 30, "lastyear" : 365 }
+DAYS_FILTERS = { "today" : 1, "lastweek" : 7, "lastmonth" : 30, "lastyear" : 365, "max" : 365 * 20 }
 #DAYS_FILTERS["weekday"] = datetime.today().weekday()+1
-GROUP_ORDER = [ "today", "weekday", "lastweek", "lastmonth" ]
+GROUP_ORDER = [ "today", "weekday", "lastweek", "lastmonth", "lastyear", "max" ]
 
 print("Exporting {} stocks".format(len(STOCKS)))
 
@@ -28,17 +28,22 @@ for stock in STOCKS:
 
 	# Process the current timegroup
 	for timegroup in GROUP_ORDER:
-		start_date = end_date - timedelta(days=DAYS_FILTERS[timegroup])
+		# Determine the date to start the filter
+		filter_start = end_date - timedelta(days=DAYS_FILTERS[timegroup])
+
+		# Filter the results
+		today_df = df[filter_start:end_date]
+
+		# Find the first date of the filter
+		start_date = filter_start if filter_start > today_df.index.min() else today_df.index.min()
 
 		print("  Processing {:10} {} -> {}".format(timegroup+':', start_date, end_date))
-		# Filter the results
-		today_df = df[start_date:end_date]
-
+	
 		# Create the display canvas
 		plt.figure(figsize = (50,30))
 		plt.rcParams.update({'font.size': 50})
 
-		plt.title('{} for {}'.format(timegroup, stock))
+		plt.title('{} {}: {} -> {}'.format(stock, timegroup, start_date.date(), end_date.date()))
 		figure = plt.plot(today_df['Open'].tolist() if timegroup != "today" else today_df['Open'], 'xkcd:crimson', linewidth=2.5)
 		plt.savefig(intraday.get_imagefile(stock, timegroup, imagedir))
 		plt.close()	
